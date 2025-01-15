1b7b51b0437f8ae7cb3e1f649ac101e31b03ccaf
Sagnik Biswas <clicksagnikbiswas01@gmail.com>
2025-01-15 17:58:33 +0530

    main updt

--- main.go
+++ main.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello Sagnik Biswas")
+	fmt.Println("Hello, Sagnik Biswas.")
+
+	// var val_a string
+	// var val_int int32
+	// var val_float float32
+	// var val_complex complex64
+
+	// val_a = "ABC"
+	// val_int = 123
+	// val_float = 15.56
+	// val_complex = 10.25 + 4i
+
+	// fmt.Println(val_complex, val_a, val_int, val_float)
+
+	// var a *int = 10
+
+	// for i := 1; i<100; i++ {
+	// 	fmt.Println(i," ABC")
+	// }
+
+
+	i := 1;
+	for  i<1000 {
+		fmt.Println(i," ABC")
+		i = i+1 
+	}
 }
+
+
